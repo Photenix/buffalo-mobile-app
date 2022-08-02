a5495525084a0a8726a17286f1b90bcb210cd7ea
Sebastián Andrés Zapateiro Núñez <zapateirosebastian@hotmail.com>
2022-08-02 23:05:58 +0000

    Gerando archivos

---
+++ src/containers/StartPage.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import '../styles/sass/startPage.scss'
+
+function StartPage() {
+  return (
+    <h1>StartPage</h1>
+  )
+}
+
+export default StartPage
--- src/routers/AppRouters.jsx
+++ src/routers/AppRouters.jsx
@@ -1,27 +1,17 @@
 import React, { useContext, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StartPage from '../containers/StartPage';
 
-import NavBarMovil from '../modules/NavBarMovil';
 
 const AppRouters = () => {
 
-    const { isNavBar } = useContext( General )
-
-    useEffect( () =>{
-        //console.log( isNavBar );
-    },[ isNavBar ])
-
+  
     return (
         <BrowserRouter>
-            {   
-            
-                isNavBar 
-                    ?<NavBarMovil/>
-                    :''
-            }
-            
+
             <Routes>
-                <Route path="/" element={ <div>Hello</div> }/>
+                <Route path="/" element={<StartPage />} />
+
             </Routes>
         </BrowserRouter>
     );
