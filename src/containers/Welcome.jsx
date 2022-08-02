222b7368aabd0ad8e81aad221a4a9a4ab0a55e9b
Photenix <juanmanuelpinor@gmail.com>
2022-08-02 13:08:20 -0500

    Welcome

--- src/containers/Welcome.jsx
+++ src/containers/Welcome.jsx
@@ -4,6 +4,11 @@ import React, { useEffect, useState } from 'react';
 import welcome_2 from '../../asset/2.png';
 import welcome_3 from '../../asset/3.png'; */
 
+import logo from '../assets/Logo.png'
+import man1 from '../assets/man_1.png'
+import man2 from '../assets/man_2.png'
+import man3 from '../assets/man_3.png'
+
 import '../styles/sass/Welcome.scss'
 
 
@@ -16,8 +21,7 @@ const Welcome = () => {
     const upPoint = () =>{
         setPoint( point + 1 )
         if(  point + 1 < document.getElementById( "welcome-img" ).childNodes.length ){
-            cleanClass("welcome-img")
-            cleanClass("welcome-points")
+            clearClassAll()
             whatActive( point + 1 )
         }else{
             sessionStorage.seeWelcome = true
@@ -27,12 +31,17 @@ const Welcome = () => {
     
     const toPoint = ( event ) =>{
         if(  point < document.getElementById( "welcome-img" ).childNodes.length ){
-            cleanClass("welcome-img")
-            cleanClass("welcome-points")
+            clearClassAll()
             setPoint( parseInt( event.target.value ) )
             whatActive( parseInt( event.target.value ) )
         }
         setPoint( parseInt( event.target.value ) )
+    }
+
+    const clearClassAll = () => {
+        cleanClass("welcome-img")
+        cleanClass("welcome-points")
+        cleanClass("welcome-text")
     }
 
     const cleanClass = ( id ) =>{
@@ -45,19 +54,31 @@ const Welcome = () => {
             .childNodes[ id ].className = "active"
         document.getElementById( "welcome-img" )
             .childNodes[ id ].className = "active"
+        document.getElementById( "welcome-text" )
+            .childNodes[ id ].className = 'active'
     }
 
     return (
         <div className='welcome'>
                 <div className="welcome-img" id='welcome-img'>
-                    <img src={ 'hi' } alt="" className='active'/>
-                    <img src={ 'hi' } alt="" />
-                    <img src={ 'hi' } alt="" />
+                    <img src={ man1 } alt="" className='active'/>
+                    <img src={ man2 } alt="" />
+                    <img src={ man3 } alt="" />
                 </div>
                 <div className="welcome-footer">
-                    <div className="welcome-text">
-                        <h1>Test</h1>
-                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi maiores</h2>
+                    <div className="welcome-text" id='welcome-text'>
+                        <div className="active">
+                            <h1>Test 1</h1>
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi maiores</h2>
+                        </div>
+                        <div>
+                            <h1>Test 2</h1>
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi maiores</h2>
+                        </div>
+                        <div>
+                            <h1>Test 3</h1>
+                            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi maiores</h2>
+                        </div>
                     </div>
                     <div className="welcome-points" id='welcome-points'>
                         <button className='active' value={0} onClick={ toPoint }></button>
